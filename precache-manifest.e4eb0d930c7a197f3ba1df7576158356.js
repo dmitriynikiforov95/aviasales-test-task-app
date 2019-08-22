18b91fe4647003f8c485b8fd37abdf0d45c26205
Dmitriy Nikiforov <nikiforov.psy@gmail.com>
2019-08-22 16:49:16 +0300

    Updates

---
+++ precache-manifest.e4eb0d930c7a197f3ba1df7576158356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64d7681eec1ca28ebd2b619ac8f12c4",
+    "url": "/aviasales-test-task-app/index.html"
+  },
+  {
+    "revision": "715e31607f6ead0babf4",
+    "url": "/aviasales-test-task-app/static/css/main.340d11bd.chunk.css"
+  },
+  {
+    "revision": "f476e3841cae4c2b55fd",
+    "url": "/aviasales-test-task-app/static/js/2.860d2ea0.chunk.js"
+  },
+  {
+    "revision": "715e31607f6ead0babf4",
+    "url": "/aviasales-test-task-app/static/js/main.e4c0684d.chunk.js"
+  },
+  {
+    "revision": "9f4254d52423d7bdc4fb",
+    "url": "/aviasales-test-task-app/static/js/runtime~main.ba260aca.js"
+  },
+  {
+    "revision": "96990a1b3638b68f706e0a3331be2821",
+    "url": "/aviasales-test-task-app/static/media/Logo.96990a1b.svg"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/aviasales-test-task-app/static/media/Shape.68ca57dc.svg"
+  }
+]);
